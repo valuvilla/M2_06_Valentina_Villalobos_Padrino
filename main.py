10180471ab577da509750578366dd50027dff325
valuvilla <valuvp@hotmail.com>
2022-10-19 16:07:38 +0200

    .

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+def lee_numero():
+    numero1=int(input("Dígito: "))    
+    numero2=int(input("Dígito: "))  
+    numero3=int(input("Dígito: "))     
+    return numero1, numero2, numero3
+
+
+
+def mayor(numero1, numero2, numero3):
+    numero1, numero2, numero3 = lee_numero()
+    if numero1 < numero2 < numero3:
+        return "Mayor {}".format(numero3)
+    elif numero3 < numero1 < numero2:
+        return "Mayor {}".format(numero2)
+    if numero2 < numero3 < numero1:
+        return "Mayor {}".format(numero1)
+
+
+
+
+    
+    
+
+
+
