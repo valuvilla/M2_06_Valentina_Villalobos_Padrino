951b18c5c0c3dc1d1d0d4f129079b1d01a4330fa
valuvilla <valuvp@hotmail.com>
2022-10-21 18:20:24 +0200

    .

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 
 
 def lee_numero():
+    valores=[]
     numero1=int(input("Dígito: "))    
     numero2=int(input("Dígito: "))  
-    numero3=int(input("Dígito: "))     
+    numero3=int(input("Dígito: "))
+    valores.append(numero1, numero2, numero3)  
+    print(valores)
     return numero1, numero2, numero3
 
 
 
 def mayor():
     numero1, numero2, numero3 = lee_numero()
-    if numero1 < numero2 < numero3:
-        return "Mayor: {}".format(numero3)
-    elif numero3 < numero1 < numero2:
-        return "Mayor: {}".format(numero2)
-    if numero2 < numero3 < numero1:
-        return "Mayor: {}".format(numero1)
+    mayor=max(numero1,numero2, numero3)
+    return "El mayor numero es el {}".format(mayor)
+
 
 mayor()
 
