74432f150f07c43b5e89732124c2c88c10e2fe54
valuvilla <valuvp@hotmail.com>
2022-10-22 15:00:44 +0200

    ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,8 +2,8 @@ import sys
 #Definimos la funcion que determinara el estado nutricional de la persona
 def imc():
     #Pedimos por teclado la talla y peso
-    altura=float(input("talla: "))
-    peso_total=float(input("peso: "))
+    altura=float(input("talla en metros: "))
+    peso_total=float(input("peso en kg: "))
 
     #Acortamos los decimales del precio
     peso=float("{:.02f}".format(peso_total))
@@ -13,12 +13,12 @@ def imc():
 
     #Comparamos resultados
     if IMC< 18.5:
-        return "Su IMC es {}: Bajo de peso con un peso de {}kg y una talla de {}m".format(IMC,peso, altura)
+        return "IMC: {}.Esto implica bajo de peso con un peso de {}kg y una talla de {}m".format(IMC,peso, altura)
     elif 18.5<IMC<25:
-        return "Su IMC es {}: Normal con un peso de {}kg y una talla de {}m".format(IMC, peso, altura)
+        return "IMC: {}. Esto implica normal con un peso de {}kg y una talla de {}m".format(IMC, peso, altura)
     elif 25<=IMC<30:
-        return "Su IMC es {}: Sobrepeso con un peso de {}kg y una talla de {}m".format(IMC, peso, altura)
+        return "IMC: {}. Esto implica sobrepeso con un peso de {}kg y una talla de {}m".format(IMC, peso, altura)
     else:
-        return "Su IMC es {}: Obesidad con un peso de {}kg y una talla de {}m".format(IMC,peso, altura)
+        return "IMC: {}. Esto implica obesidad con un peso de {}kg y una talla de {}m".format(IMC,peso, altura)
 
 print(imc())
